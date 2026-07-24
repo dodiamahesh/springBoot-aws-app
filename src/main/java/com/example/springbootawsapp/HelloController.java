bd34ed265fc8be506fe3ae4d435e0f4dcca9c7fe
dodiamahesh <maheshjdodia@gmail.com>
2026-07-24 23:46:04 +0530

    did change for ci/cd testing with autoconfigure Jenkin pipeline
    did change for ci/cd testing with autoconfigure Jenkin pipeline

--- src/main/java/com/example/springbootawsapp/HelloController.java
+++ src/main/java/com/example/springbootawsapp/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello World from Spring Boot!";
+        return "Hello Dhairyaraj Dodia from AWS!";
     }
 
     @GetMapping("/")
